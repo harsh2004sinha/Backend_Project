a53bd1f7a7d966550430f33e7a223793e8023258
Harsh-Sinha <harsh2004sinha@gmail.com>
2024-12-13 13:27:15 +0530

    completed utils folder

--- src/index.js
+++ src/index.js
@@ -6,4 +6,19 @@ dotenv.config({
     path: "./env"
 })
 
-connectDB();
+connectDB()
+.then(() => {
+    const port = process.env.PORT || 8000
+    app.listen(port, () => {
+        console.log(`Server is running at port: ${port}`);
+    })
+
+    app.on("error", (err) => {
+        console.log("ERR: ", err);
+        throw err
+    })
+})
+.catch((err) => {
+    console.log("MongoDB connection error !!!", err);
+    
+})
